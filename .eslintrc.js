246b1d9a76dd96ef0e63b3cd91d89b89051e283e
snakewarhead <snakewarhead@gmail.com>
2023-07-24 21:53:24 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  extends: ['standard', 'plugin:prettier/recommended', 'plugin:node/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'node/no-unpublished-require': ['error', { devDependencies: true }],
+  },
+}
