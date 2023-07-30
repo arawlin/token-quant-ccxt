49de6f402ca5ab863e3f422a64a10978c5251a02
snakewarhead <snakewarhead@gmail.com>
2023-07-30 17:25:37 +0800

    future_prepare 2, merged

--- python/strategy/dynamic_balance/future.py
+++ python/strategy/dynamic_balance/future.py
@@ -22,6 +22,26 @@ MARGIN_TYPE = "isolated"
 LEVERAGE = 1
 SIDE_POSITION = "long"
 
+
+def init():
+    ex = ccxt.binanceusdm(
+        {
+            "apiKey": os.getenv("API_KEY"),
+            "secret": os.getenv("API_SECRET"),
+        }
+    )
+    # ex.http_proxy = "http://192.168.1.100:1083/"
+    # ex.https_proxy = "http://192.168.1.100:1083/"
+    # ex.socks_proxy = "socks5://192.168.1.100:1082/"
+    # ex.verbose = True
+
+    ex.set_margin_mode(MARGIN_TYPE, SYMBOL)
+    ex.set_leverage(LEVERAGE, SYMBOL)
+    # ex.set_position_mode(hedged=True, symbol=SYMBOL)
+
+    return ex
+
+
 RATE_VALUE_MID = 0.5
 RATE_VALUE_DELTA = 0.0005
 
@@ -31,26 +51,11 @@ INTERVAL_TICKER = 5
 INTERVAL_TICKER_QUICK_RATE = 2
 INTERVAL_OPEN_ORDER_WAIT = 2
 
-ex = ccxt.binanceusdm(
-    {
-        "apiKey": os.getenv("API_KEY"),
-        "secret": os.getenv("API_SECRET"),
-    }
-)
-# ex.http_proxy = "http://192.168.1.100:1083/"
-# ex.https_proxy = "http://192.168.1.100:1083/"
-# ex.socks_proxy = "socks5://192.168.1.100:1082/"
-# ex.verbose = True
-
-ex.set_margin_mode(MARGIN_TYPE, SYMBOL)
-ex.set_leverage(LEVERAGE, SYMBOL)
-# ex.set_position_mode(hedged=True, symbol=SYMBOL)
-
 volume_total = 0
 interval_ticker_cur = INTERVAL_TICKER  # the interval will be quick when trading
 
 
-def update_balance(side_position="long"):
+def update_balance(ex, side_position="long"):
     global volume_total
     global interval_ticker_cur
 
@@ -138,8 +143,48 @@ def update_balance(side_position="long"):
         print(e)
 
 
+def calc_grid_price(price, rate, num):
+    """
+    geometric
+    """
+    prices = []
+    p = price
+    for i in range(num):
+        p *= 1 + rate
+        prices.append(p)
+    return prices
+
+
+def calc_grid_quantity(price, rate, amount, num, grids):
+    num -= 1
+    if num < 0:
+        return
+
+    price = price * (1 + rate)
+
+    a = amount * rate / 2
+    amount += a
+
+    q = -(a / price)
+
+    grids.append([price, q])
+    calc_grid_quantity(price, rate, amount, num, grids)
+
+
 if __name__ == "__main__":
+    # print(calc_grid_price(1000, 0.01, 10))
+    # print(calc_grid_price(1000, -0.01, 10))
+
+    # grids = []
+    # calc_grid_quantity(1000, 0.01, 1000, 10, grids)
+    # print(len(grids), grids)
+
+    # grids = []
+    # calc_grid_quantity(1000, -0.01, 1000, 10, grids)
+    # print(grids)
+
+    ex = init()
     while 1:
-        update_balance(SIDE_POSITION)
+        update_balance(ex, SIDE_POSITION)
         time.sleep(interval_ticker_cur)
         interval_ticker_cur = INTERVAL_TICKER
